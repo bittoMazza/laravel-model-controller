b6de9ad264a6040e5d3c8e69e24b27841f2c0029
MyUsername <robertomazza99@gmail.com>
2022-09-08 16:24:21 +0200

    add some style on topMovies page

--- resources/views/topMovies.blade.php
+++ resources/views/topMovies.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 
 @section('main-content')
+    <h2 class="text-center pt-5 display-2">TOP 3 FILM</h2>
     <div class="cards-container d-flex flex-wrap gap-4 justify-content-center">
         @forelse ($movies as $movie)
             @include('includes.movieCards')
